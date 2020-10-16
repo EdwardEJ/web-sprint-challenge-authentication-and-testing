53da72e09f64f5b92c22d8bdb8fa1e809b97b154
A <jesus.e.estrada@hotmail.com>
2020-10-16 12:11:53 -0500

    testing

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,26 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
+
+const { jwtSecret } = require('../api/config');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, jwtSecret, (err, decodedToken) => {
+      if (err) {
+        //token is invalid
+        res.status(401).json({ you: 'dont have the right token' });
+      } else {
+        //token is valid
+        req.jwt = decodedToken;
+
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
--- knexfile.js
+++ knexfile.js
@@ -11,4 +11,18 @@ module.exports = {
       directory: './database/seeds',
     },
   },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
 };
